b8b272ce552c2a56d1a897536afee20f12fdb4a0
AustinMusuya <amusuya57@gmail.com>
2024-12-15 17:38:39 +0300

    create Event model to hold event details

--- event_project/apis/models.py
+++ event_project/apis/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.utils.timezone import now
+from django.core.exceptions import ValidationError
+from django.contrib.auth import get_user_model
 
 # Create your models here.
+User = get_user_model()
+class Event(models.Model):
+    title = models.CharField(max_length=150)
+    description = models.TextField()
+    date = models.DateTimeField()
+    location = models.CharField(max_length=150)
+    price = models.FloatField(default=0.00)
+    requires_payment = models.BooleanField(default=False)
+    requires_rsvp = models.BooleanField(default=False)
+    organizer = models.ForeignKey(User, on_delete=models.CASCADE, related_name='event_organizer')
+
+    def clean(self):
+        if self.date.date() <= now().date():
+            raise ValidationError("The event date must be set at least one day in the future.")
+        
+    def save(self, *args, **kwargs):
+        self.full_clean()  # Call clean() before saving
+        super().save(*args, **kwargs)
