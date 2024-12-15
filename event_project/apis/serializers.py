6f2d6529fa030ce6695f52ca39d2ca3d78e89d90
AustinMusuya <amusuya57@gmail.com>
2024-12-15 19:02:25 +0300

    create serializers for user registration, and event model and user login

--- event_project/apis/serializers.py
+++ event_project/apis/serializers.py
@@ -1,4 +1,41 @@
 from django.contrib.auth import get_user_model
+from .models import Event
+from rest_framework import serializers
+from rest_framework.authtoken.models import Token
 
 User = get_user_model()
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'email', 'password']
+
+
+class RegisterUserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'email', 'password']
+
+    def save(self, **kwargs):
+        new_user = User.objects.create_user(
+            username = self.validated_data['username'],
+            email = self.validated_data['email'],
+            password = self.validated_data['password']
+        )
+
+        new_user.save()
+        # Generate token for the new user
+        token = Token.objects.get_or_create(user=new_user)
+
+
+class LoginSerializer(serializers.ModelSerializer):
+    username = serializers.CharField()
+    password = serializers.CharField()
+    class Meta:
+        model = User
+        fields = ['username', 'password']
+class EventSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Event
+        fields = '__all__'
 
