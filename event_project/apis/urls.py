79dba22feac355886497d1b20296bc346026ea65
AustinMusuya <amusuya57@gmail.com>
2024-12-15 17:39:51 +0300

    create urls paths to views for creating listing events and user registration and login

---
+++ event_project/apis/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path, include
+from . import views
+
+urlpatterns = [
+    # path('events/create-event',views.CreateEventAPIView.as_view(),name="create-event"),
+    # path('events/list-event',views.CreateEventAPIView.as_view(),name="list-event"),
+    # path('events/<int:id>',views.CreateEventAPIView.as_view(),name="detail-event"),
+    # path('users/register',views.CreateEventAPIView.as_view(),name="register"),
+    # path('users/login',views.CreateEventAPIView.as_view(),name="login"),
+    # path('events/upcoming',views.CreateEventAPIView.as_view(),name="upcoming-events"),
+]
