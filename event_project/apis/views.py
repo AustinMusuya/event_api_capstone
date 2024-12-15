0f828801d796533e9efff016c4ac3296e518ed82
AustinMusuya <amusuya57@gmail.com>
2024-12-15 19:04:06 +0300

    add APIViews for login and user registration

--- event_project/apis/views.py
+++ event_project/apis/views.py
@@ -1,3 +1,79 @@
 from django.shortcuts import render
+from django.contrib.auth import get_user_model
+from rest_framework import serializers
+from rest_framework import views, status
+from rest_framework.authentication import TokenAuthentication, SessionAuthentication, authenticate
+from rest_framework.authtoken.models import Token
+from rest_framework.response import Response
+from rest_framework.authtoken.views import ObtainAuthToken
+from .serializers import LoginSerializer, RegisterUserSerializer, EventSerializer
 
 # Create your views here.
+User = get_user_model()
+class RegisterUserAPIView(views.APIView):
+    serializer_class = RegisterUserSerializer
+
+    def get(self, request):
+        return Response(
+            {
+                'message': 'Use POST request with username, email & password to register new user'
+            },
+            status=status.HTTP_200_OK
+        )
+
+    def post(self, request):
+        serializer = self.serializer_class(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+
+            user = User.objects.get(username=request.data['username'])
+            token = Token.objects.get(user=user)
+
+            serializer = self.serializer_class(user)
+
+            return Response(
+                {
+                    'message': 'New user Registration successful!',
+                    'user': serializer.data,
+                    'token': token.key
+                },
+                status=status.HTTP_201_CREATED
+            )
+        
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+class LoginUserAPIView(views.APIView):
+    serializer_class = LoginSerializer
+
+    def get(self, request):
+        return Response(
+            {
+                "message": "Use POST request with email & password to login user"
+            },
+            status=status.HTTP_200_OK
+        )
+    
+    def post(self, request):
+        serializer = self.serializer_class(data=request.data)
+        if serializer.is_valid(raise_exception=True):
+            user = authenticate(
+                username=serializer.validated_data['username'],
+                password=serializer.validated_data['password']
+            )
+
+            if user:
+                authenticated_user = User.objects.get(username=request.data['username'])
+
+                serializer = self.serializer_class(authenticated_user)
+
+                token, created = Token.objects.get_or_create(user=user)
+                return Response(
+                    {
+                        'message': 'Login succesful.',
+                        'user': serializer.data,
+                        'token': token.key
+                    },
+                    status=status.HTTP_200_OK
+                )
+
+        return Response({'Message': 'Invalid Username and Password'}, status=status.HTTP_401_UNAUTHORIZED)
