8e6ff28cdd17d3fdd2d58bb9564bfafb526246a0
AustinMusuya <amusuya57@gmail.com>
2025-01-07 14:56:48 +0300

    import CreateEventSerializer class. alter List event Api view. create CreateEventAPIView to handle logic for user creating an event

--- event_project/apis/views.py
+++ event_project/apis/views.py
@@ -10,7 +10,7 @@ from rest_framework.authtoken.models import Token
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.authtoken.views import ObtainAuthToken
-from .serializers import LoginSerializer, RegisterUserSerializer, EventSerializer, UserSerializer
+from .serializers import LoginSerializer, RegisterUserSerializer, EventSerializer, UserSerializer, CreateEventSerializer
 
 # Create your views here.
 User = get_user_model()
@@ -99,19 +99,57 @@ class LogoutAPIView(views.APIView):
     
 
 # APIView to List all events
-class ListEventAPIView(generics.ListAPIView):
+class ListEventAPIView(views.APIView):
     serializer_class = EventSerializer
-    queryset = Event.objects.all()
-    authentication_classes = [TokenAuthentication]
+    authentication_classes = [SessionAuthentication, TokenAuthentication]
     permission_classes = [IsAuthenticated]
 
+    def get(self, request):
+        events = Event.objects.all()
+
+        if events.exists():
+            serializer = self.serializer_class(events, many=True)
+
+            return Response(
+                {
+                    'events':serializer.data,
+                },
+                status=status.HTTP_200_OK
+            )
+        return Response({'Message': 'No event records available'}, status=status.HTTP_404_NOT_FOUND)
 
 # APIView to Create an event
-class CreateEventAPIView(generics.CreateAPIView):
-    serializer_class = EventSerializer
-    queryset = Event.objects.all()
+class CreateEventAPIView(views.APIView):
+    serializer_class = CreateEventSerializer
     authentication_classes = [TokenAuthentication]
     permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        return Response(
+            {
+                'message': 'Use POST request with title, description, date, location & ticket_price to create a new event.'
+            },
+            status=status.HTTP_200_OK
+        )
+    
+    def post(self, request):
+        serializer = self.serializer_class(data=request.data)
+        if serializer.is_valid():
+            serializer.save(organizer=request.user) 
+
+            event = Event.objects.get(title=request.data['title'])
+
+            new_event_serializer = self.serializer_class(event)
+
+            return Response(
+                {
+                    'event': new_event_serializer.data,
+                    'message': "New event created successfully!"
+                },
+                status=status.HTTP_201_CREATED
+            )
+        
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 # APIView to Retrieve a specific event
