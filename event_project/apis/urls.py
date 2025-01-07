a35d3cc218bca12ff653eb5ad0259e9bfb18750d
AustinMusuya <amusuya57@gmail.com>
2025-01-07 06:51:33 +0300

    create urls for event crud operations

--- event_project/apis/urls.py
+++ event_project/apis/urls.py
@@ -2,14 +2,19 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    # path('events/create-event',views.CreateEventAPIView.as_view(),name="create-event"),
-    # path('events/list-event',views.CreateEventAPIView.as_view(),name="list-event"),
-    # path('events/<int:id>',views.CreateEventAPIView.as_view(),name="detail-event"),
-    # path('events/upcoming',views.CreateEventAPIView.as_view(),name="upcoming-events"),
-
     # views for user registration, login & logout
     path('users/register/',views.RegisterUserAPIView.as_view(),name="register"),
     path('users/login/',views.LoginUserAPIView.as_view(),name="login"),
     path('users/logout/',views.LogoutAPIView.as_view(),name="logout"),
+
+    # CRUD views for events and users
+    path('events/create-event/',views.CreateEventAPIView.as_view(),name="create-event"),
+    path('events/list-events/',views.ListEventAPIView.as_view(),name="list-event"),
+    path('events/<int:pk>/',views.RetrieveEventAPIView.as_view(),name="detail-event"),
+    path('events/<int:pk>/edit/',views.RetrieveUpdateEventAPIView.as_view(),name="edit-event"),
+    path('events/<int:pk>/delete/',views.RetrieveDestroyEventAPIView.as_view(),name="delete-event"),
+    
+    # path('events/upcoming',views.CreateEventAPIView.as_view(),name="upcoming-events"),
+
     
 ]
