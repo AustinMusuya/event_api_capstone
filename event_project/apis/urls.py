7d9a4419ed9cee8b6d0d7dd0df13dcecb16915a3
AustinMusuya <amusuya57@gmail.com>
2025-01-08 17:42:37 +0300

    update path urls for retrieving updating and deleting a specific event to one  single view

--- event_project/apis/urls.py
+++ event_project/apis/urls.py
@@ -10,9 +10,9 @@ urlpatterns = [
     # CRUD views for events and users
     path('events/create-event/',views.CreateEventAPIView.as_view(),name="create-event"),
     path('events/list-events/',views.ListEventAPIView.as_view(),name="list-event"),
-    path('events/<int:pk>/',views.RetrieveEventAPIView.as_view(),name="detail-event"),
-    path('events/<int:pk>/edit/',views.RetrieveUpdateEventAPIView.as_view(),name="edit-event"),
-    path('events/<int:pk>/delete/',views.RetrieveDestroyEventAPIView.as_view(),name="delete-event"),
+    path('events/<int:pk>/',views.RetrieveUpdateDeleteEventAPIView.as_view(),name="detail-event"),
+    path('events/<int:pk>/edit/',views.RetrieveUpdateDeleteEventAPIView.as_view(),name="edit-event"),
+    path('events/<int:pk>/delete/',views.RetrieveUpdateDeleteEventAPIView.as_view(),name="delete-event"),
     
     path('events/upcoming/',views.ListEventUpcomingAPIView.as_view(),name="upcoming-events"),
 
