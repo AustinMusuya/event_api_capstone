781314858ca5ec3fdd6596704f4e2b0db077af97
AustinMusuya <amusuya57@gmail.com>
2025-01-10 02:02:30 +0300

    add tags to event model

--- event_project/apis/models.py
+++ event_project/apis/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.utils.timezone import now
 from django.core.exceptions import ValidationError
 from django.contrib.auth import get_user_model
+from taggit.managers import TaggableManager
 
 # Create your models here.
 User = get_user_model()
@@ -11,4 +12,5 @@ class Event(models.Model):
     date = models.DateTimeField()
     location = models.CharField(max_length=150)
     ticket_price = models.FloatField(default=0.00)
+    tags = TaggableManager()
     organizer = models.ForeignKey(User, on_delete=models.CASCADE, related_name='event_organizer')
